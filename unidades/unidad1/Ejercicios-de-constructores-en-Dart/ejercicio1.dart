d591cf4be7e925390097093d4ed977996ac6f8ba
mackstm <thelewyntm@gmail.com>
2025-09-26 19:02:19 +0100

    tarea4

---
+++ unidades/unidad1/Ejercicios-de-constructores-en-Dart/ejercicio1.dart
@@ -0,0 +1,10 @@
+/**
+ * 1) Constructor normal
+ * Crea una clase Pelicula con atributos titulo y anio.
+ *     Haz un constructor normal que reciba ambos.
+ *     En main, crea una película y muestra sus datos.
+ */
+
+void main(List<String> args) {
+  
+}
