fe0f995ba42fe3ccc810243cf0cce023a6a33944
danielrguezh <danielrh111005@gmail.com>
2025-11-26 16:22:00 +0000

    cuestionario

---
+++ unidades/unidad2/cuestionario_nav/lib/ejercicios/e6.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Tema claro/oscuro',
+      home: Scaffold(
+        appBar: AppBar(title: const Text('Home Page')),
+
+        body: Center(
+          child: ElevatedButton(onPressed: () {}, child: Text('Botón')),
+        ),
+
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          child: Icon(Icons.light_mode),
+        ),
+      ),
+    );
+  }
+}
---
+++ unidades/unidad2/nav_escape_room/android/app/src/main/kotlin/com/example/nav_escape_room/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.nav_escape_room
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
