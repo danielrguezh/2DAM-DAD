af5187d572eb2e7606fed3fb89c55f13b8e78f9f
danielrguezh <danielrh111005@gmail.com>
2025-12-15 15:25:59 +0000

    anim

---
+++ unidades/unidad3/animaciones_hero/lib/ejercicios/e1.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: const FirstPage());
+  }
+}
+
+class FirstPage extends StatelessWidget {
+  const FirstPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Pantalla 1')),
+      body: Center(
+        child: 
+Respuesta 1 Pregunta 1
+
+      ),
+    );
+  }
+}
+
+class SecondPage extends StatelessWidget {
+  const SecondPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Pantalla 2')),
+      body: Center(
+        child: 
+Respuesta 2 Pregunta 1
+
+      ),
+    );
+  }
+}
+  
+
